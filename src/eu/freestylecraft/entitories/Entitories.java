2e8984174c57e6c85b4a889fc33230f84a14ef9e
CraftKekser <craftkekser123@gmail.com>
2020-08-05 23:19:12 +0200

    Basic project setup

---
+++ src/eu/freestylecraft/entitories/Entitories.java
@@ -0,0 +1,17 @@
+package eu.freestylecraft.entitories;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Entitories extends JavaPlugin{
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+
+	}
+	
+}
